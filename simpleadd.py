d6610bbd453613ebe25cc13d8d9c41d87b916035
Syed <syedaseeka@hotmail.com>
2024-10-19 02:00:53 -0700

    Add files via upload

---
+++ simpleadd.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    return a + b
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Call the function and display the result
+result = add_numbers(num1, num2)
+print("Sum:", result)
